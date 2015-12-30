734a24748639d608927683a0de3941d7a7c51300
Mark Coker <mark@cokers.com.au>
2015-12-30 21:16:00 +1100

    Fixed code escaping issue

--- integrity.sh
+++ integrity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #example: ./integrity.sh https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
-#output: <script src=https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js integrity="sha256-ivk71nXhz9nsyFDoYoGf2sbjrR9ddh+XDkCcfZxjvcM= sha512-7aMbXH03HUs6zO1R+pLyekF1FTF89Deq4JpHw6zIo2vbtaXnDw+/2C03Je30WFDd6MpSwg+aLW4Di46qzu488Q=="></script>
+#output: <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js" integrity="sha256-ivk71nXhz9nsyFDoYoGf2sbjrR9ddh+XDkCcfZxjvcM= sha512-7aMbXH03HUs6zO1R+pLyekF1FTF89Deq4JpHw6zIo2vbtaXnDw+/2C03Je30WFDd6MpSwg+aLW4Di46qzu488Q=="></script>
 slashes=$(echo $1 | awk -F'/' '{ print NF }')
 file=$(echo $1 | cut -d / -f $slashes)
 checked=false
@@ -25,7 +25,7 @@ do
 		checked=true
 		clear
 		printf "Success!\n\nCopy the following html snippet:\n\n"
-		printf "<script src="$1" integrity=\"sha256-"$firstSHA256" sha512-"$firstSHA512"\"></script>\n\n"
+		printf "<script src=\""$1"\" integrity=\"sha256-"$firstSHA256" sha512-"$firstSHA512"\"></script>\n\n"
 	else
 		if [ $count -gt 3 ]
 		then
